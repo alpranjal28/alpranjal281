9029f6163cb68f21ff071f6f287855e151dcbb4b
alpranjal28 <144878447+alpranjal28@users.noreply.github.com>
2024-01-19 20:32:25 +0530

    starting typescript

---
+++ TypeScript/app.ts
@@ -0,0 +1,13 @@
+//tsc --init; in terminal for tsconfig.json file
+
+//string
+let cats: string = "oggy";
+console.log(cats);
+
+//number
+let age: number = 3;
+
+//array
+let names = ["ajit", "sarojini", "shlesha", "pranjal"];
+names.push("scooby");
+// names.push(9) // TS wont let this pass

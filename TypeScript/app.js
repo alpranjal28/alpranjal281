d257a15eec7ae58d5d7ac4eb7a2ee5d2b93c3e33
alpranjal28 <144878447+alpranjal28@users.noreply.github.com>
2024-01-20 21:15:19 +0530

    starting nextJS with TS

--- TypeScript/app.js
+++ TypeScript/app.js
@@ -1,1 +1,35 @@
-var cats = "kitty";
+//tsc --init; in terminal for tsconfig.json file
+//string
+var cats = "oggy";
+console.log(cats);
+//number
+var age = 3;
+//array
+var names = ["ajit", "sarojini", "shlesha", "pranjal"];
+names.push("scooby");
+// names.push(9) // TS wont let this pass
+var gameList = ["assassins creed", "grand theft auto"];
+console.log(gameList);
+//OBJECTS
+var persons = [
+    {
+        name: "ajit",
+        age: 54,
+    },
+    {
+        name: "sarojini",
+        age: 52,
+    },
+    {
+        name: "shlesha",
+        age: 22,
+    },
+    {
+        name: "pranjal",
+        age: 26,
+    },
+];
+console.log(persons[3]);
+var caller = function () {
+    return persons[2].name;
+};
--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -11,3 +11,30 @@ let age: number = 3;
 let names = ["ajit", "sarojini", "shlesha", "pranjal"];
 names.push("scooby");
 // names.push(9) // TS wont let this pass
+let gameList: string[] = ["assassins creed", "grand theft auto"];
+console.log(gameList);
+
+//OBJECTS
+let persons = [
+    {
+        name: "ajit",
+        age: 54,
+    },
+    {
+        name: "sarojini",
+        age: 52,
+    },
+    {
+        name: "shlesha",
+        age: 22,
+    },
+    {
+        name: "pranjal",
+        age: 26,
+    },
+];
+console.log(persons[3]);
+
+let caller= ()=>{
+    return persons[2].name
+}

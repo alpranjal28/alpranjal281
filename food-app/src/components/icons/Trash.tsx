ca76830406c88722d2b50b16c2f5723312ea177f
alpranjal28 <144878447+alpranjal28@users.noreply.github.com>
2024-03-22 12:51:02 +0530

    before refactoring menuItemForm

--- food-app/src/components/icons/Trash.tsx
+++ food-app/src/components/icons/Trash.tsx
@@ -1,4 +1,4 @@
-export default function Trash() {
+export default function Trash({ className = "w-6 h-6" }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -6,7 +6,7 @@ export default function Trash() {
       viewBox="0 0 24 24"
       strokeWidth="1.5"
       stroke="currentColor"
-      className="w-6 h-6"
+      className={className}
     >
       <path
         strokeLinecap="round"
